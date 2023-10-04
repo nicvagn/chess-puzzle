1f06089f393ef7a18ac5badaa78c0d4f05c4e08e
nicvagn <nicolasvaagen@gmail.com>
2023-10-04 11:22:15 -0600

    before

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
-import { useState } from 'react'
+import {useState} from 'react'
 import { Chessboard } from 'react-chessboard';
-import { Chess } from 'chess.js'
+import Chess from 'chess.js'
 
 
 function App() {
+  var rightSquare;
+
+  const game = new Chess(
+    'r1k4r/p2nb1p1/2b4p/1p1n1p2/2PP4/3Q1NB1/1P3PPP/R5K1 b - - 0 19'
+  )
+    //when a user drops a piece
+    function onDrop(source,target){
+      if(target !== rightSquare){
+        
+      }
+
+  }
   return (
-    <div className="App">
-      <Chessboard/>
+    <div className="app">
+      <Chessboard position={game.fen()}/>
     </div>
   );
 }
