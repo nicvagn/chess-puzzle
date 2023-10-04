5c6bf13de7647427fed464ba10144695ac6e064b
nicvagn <nicolasvaagen@gmail.com>
2023-10-04 11:44:41 -0600

    got it dissplaying chessboard of given fen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import './App.css';
 import {useState} from 'react'
 import { Chessboard } from 'react-chessboard';
-import Chess from 'chess.js'
+import { Chess } from 'chess.js'
 
 
 function App() {
   var rightSquare;
 
   const game = new Chess(
-    'r1k4r/p2nb1p1/2b4p/1p1n1p2/2PP4/3Q1NB1/1P3PPP/R5K1 b - - 0 19'
+    'rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2'
   )
     //when a user drops a piece
     function onDrop(source,target){
       if(target !== rightSquare){
-        
+        return true
       }
+      return false
 
   }
   return (
     <div className="app">
-      <Chessboard position={game.fen()}/>
+      <Chessboard position={game.fen()} onPieceDrop={onDrop}/>;
     </div>
+
   );
 }
 
