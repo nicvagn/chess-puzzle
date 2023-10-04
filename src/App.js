5e6586ec5dcb413cb4d571e3438a32eee12803ec
nicvagn <nicolasvaagen@gmail.com>
2023-10-04 15:37:24 -0600

    Done the chess puzzle interface

--- src/App.js
+++ src/App.js
@@ -2,40 +2,69 @@ import './App.css';
 import { Chessboard } from 'react-chessboard';
 import { Chess } from 'chess.js'
 import { parse } from '@vanillaes/csv'
+import * as ReactDOM from 'react-dom/client';
+
 
 function App() {
   var rightDest;
   var rightSource;
   var fen = null;
   var moves = null
-  var moveNumber = 0;
+  var moveNum = 0;
+  var yourMoveNum = 0;
   var csv = "0008,r6k/pp2r2p/4Rp1Q/3p4/8/1N1P2R1/PqP2bPP/7K b - - 0 24,f2g3 e6e7 b2b1 b3c1 b1c1 h6c1,1758,75,94,4412,crushing hangingPiece long middlegame,https://lichess.org/787zsVup/black#48,"
   var parsedCSV = parse(csv)
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+
 
   //get the starting fen from the parsed CSV
   fen = parsedCSV[0][1]
   //get the correct moves from the parsed CSV
   moves = parseMoves(parsedCSV[0][2])
-  console.log(moves)
+  //create a new chess.js object with the puzzle fen
+  const puzzle = new Chess(fen)
 
+  //parse the puzzle moves from string
   function parseMoves(m){
     var parsed = m.split(" ")
     return parsed;
   }
 
-  rightSource = moves[moveNumber].slice(0, 2)
-  rightDest = moves[moveNumber].slice(2,4)
-  console.log(rightSource)
-  console.log(rightDest)
+  console.log(moves)
+  //get every second move, ie your moves
+  let yourMoves = moves.filter((item, idx) => (idx + 1) % 2 !== 0);
+  console.log(yourMoves)
+
 
   //when a user drops a piece
   function onDrop(source, target) {
+    rightSource = yourMoves[yourMoveNum].slice(0, 2)
+    rightDest = yourMoves[yourMoveNum].slice(2,4)
     console.log("drop")
+    console.log(source)
+    console.log(target)
+    console.log(rightDest)
     if (source === rightSource) {
       console.log("right source")
       if (target === rightDest) {
-        console.log("right dest")
-        return true
+        puzzle.move(moves[moveNum])
+        yourMoveNum++
+        moveNum++;
+        puzzle.move(moves[moveNum])
+        moveNum++;
+        if(yourMoveNum >= yourMoves.length){
+          root.render(
+            <h1>Good job</h1>
+          )
+        }else{
+          root.render(
+          <div className="app">
+            <Chessboard position={puzzle.fen()} onPieceDrop={onDrop}/>
+          </div>
+          )
+          return true;}
+
       }
     }
     console.log("wrong")
@@ -44,7 +73,7 @@ function App() {
 
   return (
     <div className="app">
-      <Chessboard position={fen} onPieceDrop={onDrop}/>;
+      <Chessboard position={puzzle.fen()} onPieceDrop={onDrop}/>
     </div>
   );
 }
